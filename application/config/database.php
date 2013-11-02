8b92366aced6494680e0484898cae070cbdbfe94
unknown <Fractal Sublime@.(none)>
2013-11-02 19:38:17 +0100

    reverse db

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] 'bartklaster.nl.mysql';
-$db['default']['username'] ='bartklaster_nl';
-$db['default']['password'] = 'Wye@eHp2';
-$db['default']['database'] = 'bartklaster_nl';
+$db['default']['hostname'] = 'localhost';
+$db['default']['username'] = 'root';
+$db['default']['password'] = '';
+$db['default']['database'] = 'codeigniter';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
